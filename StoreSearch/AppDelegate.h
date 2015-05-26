5266d8ae40dbaaa19fa0edfb2faf07633b9d508b
chenxu89 <675269196@qq.com>
2015-05-27 06:46:57 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by 陈旭 on 5/27/15.
+//  Copyright (c) 2015 陈旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StoreSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by 陈旭 on 5/27/15.
+//  Copyright (c) 2015 陈旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
