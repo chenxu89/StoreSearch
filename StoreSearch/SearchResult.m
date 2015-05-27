5bcb7a001749942d8f86949dcc647122d19120a9
chenxu89 <675269196@qq.com>
2015-05-27 11:32:52 +0800

    29 Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.m
@@ -0,0 +1,14 @@
+//
+//  SearchResult.m
+//  StoreSearch
+//
+//  Created by 陈旭 on 5/27/15.
+//  Copyright (c) 2015 陈旭. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+
+@end
--- StoreSearch/SearchViewController.m
+++ StoreSearch/SearchViewController.m
@@ -7,31 +7,109 @@
 //
 
 #import "SearchViewController.h"
+#import "SearchResult.h"
 
-@interface SearchViewController ()
-
+@interface SearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation SearchViewController
+{
+    NSMutableArray *_searchResults;
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    if (_searchResults == nil) {
+        return 0;
+    }else if ([_searchResults count] == 0){
+        return 1;
+    }else{
+        return [_searchResults count];
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"SearchResultCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleSubtitle
+                reuseIdentifier:cellIdentifier];
+    }
+    
+    
+    if ([_searchResults count] == 0) {
+        cell.textLabel.text = @"(Nothing found)";
+        cell.detailTextLabel.text = @"";
+    }else{
+        SearchResult *searchResult = _searchResults[indexPath.row];
+        cell.textLabel.text = searchResult.name;
+        cell.detailTextLabel.text = searchResult.artistName;
+    }
+
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//self-produced, not necessary
+- (NSIndexPath *)tableView:(UITableView *)tableView
+  willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([_searchResults count] == 0) {
+        return nil;
+    }else{
+        return indexPath;
+    }
 }
 */
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    //dismiss the keyboard
+    [searchBar resignFirstResponder];
+    
+    _searchResults = [[NSMutableArray alloc] initWithCapacity:10];
+    
+    if (![searchBar.text isEqualToString:@"no"]) {
+        for (int i = 0; i < 3; i++) {
+            SearchResult *searchResult = [[SearchResult alloc] init];
+            searchResult.name = [NSString stringWithFormat:@"Fake Result %d for", i];
+            searchResult.artistName = searchBar.text;
+            [_searchResults addObject:searchResult];
+        }
+    }
+
+    [self.tableView reloadData];
+}
+
+- (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar
+{
+    return UIBarPositionTopAttached;
+}
 
 @end
